20984546bc27b57fba65f0d3ff1e76f17501abad
David Barnes <david.barnes115@gmail.com>
2020-04-26 12:41:36 -0400

    updated logo

--- malena/dark/mail.php
+++ malena/dark/mail.php
@@ -1,51 +1,19 @@
-<?php 
-$errors = '';
-$myemail = 'creativedge.levi@gmail.com';//<-----Put Your email address here.
-if(empty($_POST['name'])  || 
-   empty($_POST['email']) || 
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
-
-$name = $_POST['name']; 
-$email_address = $_POST['email']; 
-$message = $_POST['message']; 
-
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
-
-if( empty($errors))
-{
-	$to = $myemail; 
-	$email_subject = "Contact form submission: $name";
-	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
-	
-	$headers = "From: $myemail\n"; 
-	$headers .= "Reply-To: $email_address";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	//redirect to the 'thank you' page
-	header('Location: contact-form-thank-you.html');
-} 
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
-<html>
-<head>
-	<title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
 <?php
-echo nl2br($errors);
+
+    $myAwardSpaceEmail = "your-AwardSpace-email-goes-here";
+    $myPersonalEmail = "david.barnes115@gmail.com";
+    
+    if(isset($_POST['submit'])) {
+        // $subject = $_POST['subject'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $headers = "From: Contact Form <" . $myAwardSpaceEmail . ">" . "\r\n";
+        $headers .= "Reply-To: " . $name . " <" . $email .">" . "\r\n";
+        
+        echo 'Your message was sent successfully!';
+        mail($myPersonalEmail, $message, $headers);
+    } else {
+        echo 'An error has occurred!';
+    }
 ?>
-
-
-</body>
-</html>
